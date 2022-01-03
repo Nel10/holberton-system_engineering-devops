ff845722f16a051b2c72badc9cef542aae5d961f
Nel10 <3461@holbertonschool.com>
2022-01-03 16:09:52 -0500

    update

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
-# create a file in /tmp
+# create a file
 
-file { '/tmp/school':
+file {'/tmp/school':
 ensure  => 'present',
-mode    => '0774',
+mode    => '0744',
 owner   => 'www-data',
 group   => 'www-data',
 content => 'I love Puppet',
