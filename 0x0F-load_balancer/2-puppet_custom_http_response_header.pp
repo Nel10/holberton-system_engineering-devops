88e33793e7ae53cf0497c98518ff6cf0b4e21a6e
Nel10 <3461@holbertonschool.com>
2022-01-10 23:57:06 -0500

    Update

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,13 +1,28 @@
 # Add a custom HTTP header with Puppet
 
 exec { 'update':
-    command  => 'sudo apt-get update',
-    provider => shell,
+    command  => '/user/bin/apt-get update',
 }
--> package {'nginx':
+-> package { 'nginx':
+    ensure => installed,
+    require => Exec['update'],
+}
+-> file_line { 'redirections':
     ensure => present,
     path   => '/etc/nginx/sities-available/default',
-    line   => "   location / {
-    add_header X-Server-By ${hostname};",
-    math   => '^\location / {',
+    after  => 'liste 80 default_server;',
+    line   => 'rewrite ^/redirect_me https://www.linkedin.com/in/shannel-bejarano-022801215/ permanent;',
+}
+-> file_name { 'custom http server':
+    ensure => present,
+    path => '/etc/nginx/sities-available/default',
+    after => 'liste 80 default_server;',
+    line => 'add_header X-Server-By ${hostname};',
+}
+-> file { '/var/www/html/index.html':
+    content -> 'Hello World',
+}
+-> service { 'nginx': 
+    ensure => 'running',
+    require => package['nginx'],
 }
